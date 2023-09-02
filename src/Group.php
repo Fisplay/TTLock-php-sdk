b0de26367b74f12f4b8da0b7fdf588570a639fb0
Jaakko Manitla <jaakko.mantila@fisplay.com>
2023-09-02 15:30:29 +0300

    Add group list function

--- src/Group.php
+++ src/Group.php
@@ -23,11 +23,37 @@ class Group extends TTLockAbstract
 	 */
 	public function add(string $name, int $date ) : array
 	{
-		$response = $this->client->request( 'POST', '/v3/keyboardPwd/add', [
+		$response = $this->client->request( 'POST', '/v3/group/add', [
 			'form_params' => [
 				'clientId'    => $this->clientId,
 				'accessToken' => $this->accessToken,
 				'name' => $name,
+				'date'        => $date,
+			],
+		] );
+		$body     = json_decode( $response->getBody()->getContents(), true );
+		if( $response->getStatusCode() === 200 && !isset( $body['errcode'] ) ){
+			return (array)$body;
+		} else{
+			throw new \Exception( "errcode {$body['errcode']} errmsg {$body['errmsg']} errmsg : {$body['errmsg']}" );
+		}
+	}
+
+    /**
+	 * @param int $pageNo
+	 * @param int $pageSize
+	 * @param int $date
+	 * @return array
+	 * @throws \GuzzleHttp\Exception\GuzzleException | \Exception
+	 */
+	public function list( int $pageNo, int $pageSize, int $date ) : array
+	{
+		$response = $this->client->request( 'POST', '/v3/group/list', [
+			'form_params' => [
+				'clientId'    => $this->clientId,
+				'accessToken' => $this->accessToken,
+				'pageNo'      => $pageNo,
+				'pageSize'    => $pageSize,
 				'date'        => $date,
 			],
 		] );
