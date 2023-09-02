a2d7494e406fa69b2c6807c5d2448a13eeb8df90
Jaakko Manitla <jaakko.mantila@fisplay.com>
2023-09-02 15:29:06 +0300

    Added group/add functionality

---
+++ src/Group.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ttlock;
+class Group extends TTLockAbstract
+{
+
+	/**
+	 * @var string
+	 */
+	private $accessToken = '';
+
+	public function setAccessToken( string $accessToken ) : void
+	{
+		$this->accessToken = $accessToken;
+	}
+
+	/**
+	 * @method GET
+	 * @param strint $name
+     * @param int    $date
+	 * @return array
+	 * @throws \GuzzleHttp\Exception\GuzzleException | \Exception
+	 */
+	public function add(string $name, int $date ) : array
+	{
+		$response = $this->client->request( 'POST', '/v3/keyboardPwd/add', [
+			'form_params' => [
+				'clientId'    => $this->clientId,
+				'accessToken' => $this->accessToken,
+				'name' => $name,
+				'date'        => $date,
+			],
+		] );
+		$body     = json_decode( $response->getBody()->getContents(), true );
+		if( $response->getStatusCode() === 200 && !isset( $body['errcode'] ) ){
+			return (array)$body;
+		} else{
+			throw new \Exception( "errcode {$body['errcode']} errmsg {$body['errmsg']} errmsg : {$body['errmsg']}" );
+		}
+	}
+}
