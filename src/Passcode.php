dc280fe9107763ed304f91b417e1e008cb3eb491
Jaakko Manitla <jaakko.mantila@fisplay.com>
2023-09-02 16:15:15 +0300

    Ability to add permanent pin codes

--- src/Passcode.php
+++ src/Passcode.php
@@ -135,7 +135,7 @@ class Passcode extends TTLockAbstract
 	 * @throws \GuzzleHttp\Exception\GuzzleException | \Exception
 	 * @author 韩文博
 	 */
-	public function add( int $lockId, string $keyboardPwd, string $keyboardPwdName, int $keyboardPwdType = 3, ?int $startDate, ?int $endDate, int $addType, int $date ) : array
+	public function add( int $lockId, string $keyboardPwd, string $keyboardPwdName, int $keyboardPwdType = 3, ?int $startDate = null, ?int $endDate = null, int $addType = 3, int $date = 0 ) : array
 	{
 		$response = $this->client->request( 'POST', '/v3/keyboardPwd/add', [
 			'form_params' => [
