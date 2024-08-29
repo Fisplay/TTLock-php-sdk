3b4bb4dce46eec697aad8f00dd951c8c5a05d976
Otto Kilpirinne <otto.kilpirinne@fisplay.com>
2024-08-29 13:31:50 +0300

    Add possibility to search passcodes for lock

--- src/Passcode.php
+++ src/Passcode.php
@@ -158,4 +158,37 @@ class Passcode extends TTLockAbstract
 			throw new \Exception( "errcode {$body['errcode']} errmsg {$body['errmsg']} errmsg : {$body['errmsg']}" );
 		}
 	}
+
+	/**
+	 * @method POST
+	 * @param int         $lockId
+	 * @param int         $pageNo
+	 * @param int         $pageSize
+	 * @param int         $orderBy
+	 * @param int         $date
+	 * @param string|null $searchString
+	 * @return array
+	 * @throws \GuzzleHttp\Exception\GuzzleException | \Exception
+	 */
+	public function find(int $lockId, int $pageNo, int $pageSize, int $orderBy = 0, int $date = 0, ?string $searchString = null):array
+	{
+		$response = $this->client->request( 'POST', '/v3/lock/listKeyboardPwd', [
+			'form_params' => [
+				'clientId'    => $this->clientId,
+				'accessToken' => $this->accessToken,
+				'lockId'      => $lockId,
+				'searchStr' => $searchString,
+				'pageNo'   => $pageNo,
+				'pageSize'     => $pageSize,
+				'orderBy'     => $orderBy,
+				'date'        => $date,
+			],
+		] );
+		$body     = json_decode( $response->getBody()->getContents(), true );
+		if( $response->getStatusCode() === 200 && !isset( $body['errcode'] ) ){
+			return (array)$body;
+		} else{
+			throw new \Exception( "errcode {$body['errcode']} errmsg {$body['errmsg']} errmsg : {$body['errmsg']}" );
+		}
+	}
 }
